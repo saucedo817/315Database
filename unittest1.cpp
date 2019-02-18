5af97ef38e3c55e357da7ff365565c896951e042
Kassandra Guzman <42280454+kkgzmn@users.noreply.github.com>
2019-02-17 22:57:01 -0600

    Update unittest1.cpp

--- unittest1.cpp
+++ unittest1.cpp
@@ -36,6 +36,54 @@ namespace TestFramework1
             Table* isTableEmpty == db.query("test","","");
             Assert::IsNull(isTableEmpty);
             
+            Table::MyTable t; 
+            bool isAdded = t.add(nullptr,"test");
+            Assert::AreEqual(isAdded,true);
+
+            Table::MyTable t; 
+            bool isDeleted = t.del(nullptr,"test");
+            Assert::AreEqual(isDeleted,true);
+
+            Table::MyTable t; 
+            bool isInserted = t.insert(nullptr,"test");
+            Assert::AreEqual(isInserted,true);
+
+            Table::MyTable t; 
+            vector<string> attributes == t.getAttributes();
+            Assert::IsNull(attributes);
+
+            Table::MyTable t; 
+            int tableSizeCheck = t.getSize(); 
+            Assert::IsNull(tableSizeCheck);
+
+            Table::MyTable t;
+            Record* isIterate = t.iterate(nullptr);
+            Assert::IsNull(isIterate);
+
+            Table::MyTable t; 
+            bool isKeyAssigned = t.assignKey("test");
+            Assert::AreEqual(isKeyAssigned,true);
+
+            Table::MyTable t; 
+            Table* testCrossJoin = t.crossJoin(nullptr, nullptr);
+            Assert::IsNull(testCrossJoin);
+
+            Table::MyTable t; 
+            Table* testNaturalJoin = t.naturalJoin(nullptr, nullptr);
+            Assert::IsNull(testNaturalJoin);
+
+            Table::MyTable t; 
+            int testCount = t.count("test");
+            Assert::IsNull(testCount);
+
+            Table::MyTable t; 
+            int testMin = t.min("test");
+            Assert::IsNull(testMin);
+
+            Table::MyTable t; 
+            int testMax = t.max("test");
+            Assert::IsNull(testMax);
+            
         }
         
 
