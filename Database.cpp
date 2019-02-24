7782b24763abcbd94a6ee06fbdcad5469362c6e0
saucedo817 <33112401+saucedo817@users.noreply.github.com>
2019-02-23 23:45:13 -0600

    Add files via upload

--- Database.cpp
+++ Database.cpp
@@ -9,7 +9,7 @@
 #include "Database.h"
 
 namespace Database {
-    
+    // MyDatabase functions
     void MyDataBase::add_table(Table *table, char *table_name){
         DB.insert(make_pair(table_name,table));
     }
@@ -30,28 +30,46 @@ namespace Database {
     Table* MyDataBase::get_table(char *table_name){
         return DB[table_name];
     }
-    // record functions 
+    
+    // query function
+    
+    Table MyDataBase::query(std::string SELECT, std::string FROM, std::string WHERE){
+        
+    }
+    
+    // record functions
+    MyRecord::MyRecord(int size);
+        records[size];
+    }
+
+    int MyRecord::returnSize();
+        return records.size();
+    }
+
+
+    
+    // table functions
     Table::Table (){
         numRow = 0;
         numColumn = 0;
     }
-
-
+    
+    
     void Table::addAttribute(Table* table, std::string attribute){
         // add attribute to attribute list
         attributeList.push_back(attribute);
         // create new column vector
         std::vector<std::string> newColumn;
-
+        
         for (int i = 0; i < table->numRow; i++){
             newColumn.push_back("");
         }
-
+        
         table->tableData.push_back(newColumn);
         table->numColumn++;
     }
-
-
+    
+    
     void Table::deleteAttribute(Table* table, std::string attribute){
         // find the arrubute in the atttrubute list, because location in list is the same as the location of the column in the table
         for (int i = 0; i < attributeList.size(); i++){
@@ -59,15 +77,15 @@ namespace Database {
                 int location = i;
                 table->tableData.erase(table->tableData.begin()+location);
                 table->numColumn--;
-
+                
                 // remove from attribute list too
                 table->attributeList.erase(table->attributeList.begin()+location);
                 break;
             }
         }
     }
-
-
+    
+    
     void Table::insertRecord(Table* table, std::vector<std::string> records){
         // add each string in record to it's corresponding column
         for (int i = 0; i < table->numColumn; i++){
@@ -75,31 +93,25 @@ namespace Database {
         }
         table->numRow++;
     }
-
-
+    
+    
     std::string Table::getAttributes(Table* tableName){
         std::string attributes;
         // return a comma separted string of the arrtibutes list
         for ( int i = 0; i < tableName->attributeList.size(); i++){
             attributes = attributes + ',' + tableName->attributeList[i];
         }
-
+        
         return attributes;
     }
-
-
+    
+    
     int Table::getSize(Table* tableName){
         return tableName->numRow;
     }
+    
+    
 
-    
-    
-    
-    // query function
-    
-    Table MyDataBase::query(std::string SELECT, std::string FROM, std::string WHERE){
-        
-    }
     
     
 }
